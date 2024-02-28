9e92ce0c06b03fd15b3e611cfb2562faae50f34e
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-02-28 14:16:17 +0100

    config(Na): create CORS policy

---
+++ sodium-api/src/main/java/com/sodium/api/WebConfig.java
@@ -0,0 +1,26 @@
+// WebConfig.java
+package com.sodium.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Value("${aluminium.app.url}")
+    private String aluminiumAppUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(aluminiumAppUrl)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+            }
+        };
+    }
+}
