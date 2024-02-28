fad3e98d97e21494841126a039b2cfae85d62102
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-02-28 13:35:27 +0100

    fix(Al): use vite environment variables

--- aluminium-frontend-app/src/services/messages.ts
+++ aluminium-frontend-app/src/services/messages.ts
@@ -1,19 +1,25 @@
 import { Message } from "../types/Message";
 
 export const getMessages = async () => {
-  const response = await fetch(`${process.env.SODIUM_API_URL}/messages`);
+  const response = await fetch(
+    `${import.meta.env.VITE_SODIUM_API_URL}/messages`
+  );
   const data = await response.json();
+  console.log("data: ", data);
   return data;
 };
 
 export const postMessage = async (message: Message) => {
-  const response = await fetch(`${process.env.SODIUM_API_URL}/messages`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(message),
-  });
+  const response = await fetch(
+    `${import.meta.env.VITE_SODIUM_API_URL}/messages`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    }
+  );
   const data = await response.json();
   return data;
 };
