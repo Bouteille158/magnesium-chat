870436be2fb1e25dd167656b8259aeab15202dc5
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-06 11:11:57 +0100

    feat(Na): create web socket endpoints

---
+++ sodium-api/src/main/java/com/sodium/api/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.sodium.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${aluminium.app.url}")
+    private String aluminiumAppUrl;
+
+    @Override
+    public void registerStompEndpoints(@SuppressWarnings("null") StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket").setAllowedOrigins(aluminiumAppUrl).withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(@SuppressWarnings("null") MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
