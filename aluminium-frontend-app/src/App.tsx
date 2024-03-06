5cb55753d954be9cc412d77cc3437ed8c001b5d3
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-06 11:13:23 +0100

    feat(Al): add web socket provider

--- aluminium-frontend-app/src/App.tsx
+++ aluminium-frontend-app/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import ChatWindow from "./components/chat/ChatWindow";
+import { StompSessionProvider } from "react-stomp-hooks";
+
+const websocketURL = `${import.meta.env.VITE_SODIUM_API_URL}/socket`;
 
 function App() {
+  console.log("websocketURL: ", websocketURL);
   return (
     <>
-      <ChatWindow />
+      <StompSessionProvider url={websocketURL}>
+        <ChatWindow />
+      </StompSessionProvider>
     </>
   );
 }
