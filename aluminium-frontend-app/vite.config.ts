98c7bdc2c3e530f51b70de8ac5d4833bcf469e6d
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-07 11:32:17 +0100

    feat(Al): add manifest and service worker configuration

--- aluminium-frontend-app/vite.config.ts
+++ aluminium-frontend-app/vite.config.ts
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Magnesium chat",
+        short_name: "Magnesium chat",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp(
+              "^https://fonts.(?:googleapis|gstatic).com/(.*)"
+            ),
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "google-fonts",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images",
+            },
+          },
+          {
+            urlPattern: /\.(?:js|css)$/,
+            handler: "StaleWhileRevalidate",
+          },
+          {
+            urlPattern: /^\/$/, // la page d'accueil
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "homepage",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
