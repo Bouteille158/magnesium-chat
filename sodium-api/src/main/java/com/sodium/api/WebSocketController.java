9cbd78be67d8e9a8384998b9e6c7e4e42d7d4fc8
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-07 13:55:59 +0100

    feat(Na): save new message in database

--- sodium-api/src/main/java/com/sodium/api/WebSocketController.java
+++ sodium-api/src/main/java/com/sodium/api/WebSocketController.java
@@ -1,19 +1,27 @@
 package com.sodium.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.sodium.api.entities.Message;
+import com.sodium.api.repositories.MessageRepository;
 
 @Controller
 public class WebSocketController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @MessageMapping("/send")
     @SendTo("/topic/messages")
     public Message send(Message message) {
         System.out.println("Message sent: " + message);
-        // TODO: Save message to database
+        if (message.getId() != null) {
+            throw new IllegalArgumentException("You cannot create a message with an ID");
+        }
+        messageRepository.save(message);
         return message;
     }
 }
