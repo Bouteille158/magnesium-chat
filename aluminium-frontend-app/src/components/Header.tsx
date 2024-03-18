2b47d5e91f6c247f2330a00f1413982d7c825a5d
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-18 17:49:04 +0100

    feat(Al): add notification button in header

--- aluminium-frontend-app/src/components/Header.tsx
+++ aluminium-frontend-app/src/components/Header.tsx
@@ -1,4 +1,9 @@
+import {
+  askNotificationPermission,
+  subscribeToPushNotifications,
+} from "../services/notification";
 import "./Header.scss";
+import Spacer from "./Spacer";
 
 interface HeaderProps {
   title: string;
@@ -7,7 +12,46 @@ interface HeaderProps {
 function Header(props: HeaderProps) {
   return (
     <header className="AppHeader">
-      <h2>{props.title}</h2>
+      <Spacer width="60px" />
+      <h2
+        style={{
+          flex: 1,
+        }}
+      >
+        {props.title}
+      </h2>
+      <div
+        onClick={() => {
+          askNotificationPermission().then((permission) => {
+            console.log("permission status: ", permission);
+            if (permission) {
+              subscribeToPushNotifications();
+            }
+          });
+        }}
+        style={{
+          cursor: "pointer",
+          width: "40px",
+          height: "40px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p
+          style={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            fontSize: "40px",
+          }}
+        >
+          🔔
+        </p>
+      </div>
+      <Spacer width="20px" />
     </header>
   );
 }
