f56727ed30e5ece6f18425aa32ebf92b5c67d029
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-18 11:08:31 +0100

    feat(Al): handle push notification

--- aluminium-frontend-app/src/sw.ts
+++ aluminium-frontend-app/src/sw.ts
@@ -1,36 +1,33 @@
 /// <reference lib="WebWorker" />
 
+import { precacheAndRoute } from "workbox-precaching";
+
+const sw = self as unknown as ServiceWorkerGlobalScope & typeof globalThis;
 console.log("Service Worker Loaded...");
 
-self.addEventListener("push", function (event) {
+sw.addEventListener("push", function (event: ExtendableEvent) {
+  console.warn("Push Notification Received", event);
   const pushEvent = event as PushEvent;
   if (pushEvent.data) {
     const data = pushEvent.data.json();
-    const options = {
+    console.log("Push Event data: ", data);
+    const options: NotificationOptions = {
       body: data.body,
-      icon: "images/notification-flat.png",
+      icon: "favicon-32x32.png",
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
         primaryKey: "2",
       },
-      actions: [
-        {
-          action: "explore",
-          title: "Go to the site",
-          icon: "images/checkmark.png",
-        },
-        {
-          action: "close",
-          title: "Close the notification",
-          icon: "images/xmark.png",
-        },
-      ],
+      timestamp: Date.now(),
     };
-    (event as ExtendableEvent).waitUntil(
-      (
-        self as unknown as ServiceWorkerGlobalScope
-      ).registration.showNotification(data.title, options)
-    );
+    console.log("Push Event options: ", options);
+    console.log("Push Event options: ", JSON.stringify(options));
+    console.log("self.registration: ", sw.registration);
+
+    event.waitUntil(sw.registration.showNotification(data.title, options));
+    console.log("End of push event listener");
   }
 });
+
+precacheAndRoute((self as any).__WB_MANIFEST);
