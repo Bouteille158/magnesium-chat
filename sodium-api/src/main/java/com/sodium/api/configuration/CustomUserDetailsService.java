0a1e6558304b8c1558ebe413cec7219256f91731
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-25 18:29:13 +0100

    feat(Na): create custom user details service

---
+++ sodium-api/src/main/java/com/sodium/api/configuration/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.sodium.api.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.sodium.api.models.DBUser;
+import com.sodium.api.repositories.DBUserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private DBUserRepository dbUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        DBUser user = dbUserRepository.findByUsername(username);
+
+        return new User(user.getUsername(), user.getPassword(), getGrantedAuthorities(user.getRole()));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
