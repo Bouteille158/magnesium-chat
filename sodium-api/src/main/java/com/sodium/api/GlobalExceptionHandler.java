ff081a9ec726560e9abee3ecb6ca979209cdcf0a
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-26 14:56:08 +0100

    feat(Na): create global exception handler

---
+++ sodium-api/src/main/java/com/sodium/api/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.sodium.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred: " + e.getMessage());
+    }
+}
