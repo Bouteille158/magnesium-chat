32b75faaa7882ecbdb0e30fd3e97472e0fa55c5c
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-29 16:14:26 +0100

    fix(Al): add token to request

--- aluminium-frontend-app/src/services/messages.ts
+++ aluminium-frontend-app/src/services/messages.ts
@@ -1,8 +1,15 @@
 import { Message } from "../types/Message";
+import Cookies from "js-cookie";
 
 export const getMessages = async () => {
+  // TODO use axios instead of fetch
   const response = await fetch(
-    `${import.meta.env.VITE_SODIUM_API_URL}/messages`
+    `${import.meta.env.VITE_SODIUM_API_URL}/messages`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
   );
   const data: Message[] = await response.json();
 
@@ -12,12 +19,14 @@ export const getMessages = async () => {
 };
 
 export const postMessage = async (message: Message) => {
+  // TODO use axios instead of fetch
   const response = await fetch(
     `${import.meta.env.VITE_SODIUM_API_URL}/messages`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
       },
       body: JSON.stringify(message),
     }
