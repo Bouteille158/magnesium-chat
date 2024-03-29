092c84e95350ec9b8ef1af501f12dde746466705
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-03-29 11:02:52 +0100

    feat(Al): use axios for vapid request
    permit the use of the bearer token

--- aluminium-frontend-app/src/services/vapidKeys.ts
+++ aluminium-frontend-app/src/services/vapidKeys.ts
@@ -1,20 +1,19 @@
+import axios from "axios";
 const apiURL = `${import.meta.env.VITE_SODIUM_API_URL}/api`;
 
-export function getVapidPublicKey(): Promise<string> {
-  return fetch(`${apiURL}/vapidPublicKey`, {
-    method: "GET",
-    headers: {
-      "content-type": "application/text",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.text();
+export async function getVapidPublicKey(): Promise<string | Error> {
+  console.log(
+    "Axios vapid token",
+    axios.defaults.headers.common["Authorization"]
+  );
+  return await axios
+    .get(`${apiURL}/vapidPublicKey`, {
+      headers: {
+        "content-type": "application/text",
+      },
     })
-    .then((vapidPublicKey) => {
-      console.log("vapidPublicKey returned in the request: ", vapidPublicKey);
-      return vapidPublicKey;
+    .catch((error) => {
+      console.error("Error while fetching VAPID public key: ", error);
+      return error;
     });
 }
