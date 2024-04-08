e2f2937e5e94cf3747450db0342dc29464f39019
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-08 17:51:47 +0200

    feat(Na): create request and response classes

---
+++ sodium-api/src/main/java/com/sodium/api/models/GroupChatRequestBody.java
@@ -0,0 +1,27 @@
+package com.sodium.api.models;
+
+public class GroupChatRequestBody {
+    private Integer groupChatId;
+    private Integer userId;
+
+    public GroupChatRequestBody(Integer groupChatId, Integer userId) {
+        this.groupChatId = groupChatId;
+        this.userId = userId;
+    }
+
+    public Integer getGroupChatId() {
+        return groupChatId;
+    }
+
+    public void setGroupChatId(Integer groupChatId) {
+        this.groupChatId = groupChatId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
---
+++ sodium-api/src/main/java/com/sodium/api/models/GroupChatResponse.java
@@ -0,0 +1,28 @@
+package com.sodium.api.models;
+
+public class GroupChatResponse {
+    private String message;
+    private Integer groupId;
+
+    public GroupChatResponse(String message, Integer groupId) {
+        this.message = message;
+        this.groupId = groupId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+}
