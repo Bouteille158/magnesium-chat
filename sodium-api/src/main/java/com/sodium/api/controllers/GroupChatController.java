d76e3cbdb723ed47e5651330152be20c7cfd8619
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-11 12:06:11 +0200

    fix(Na): remove user sensitive data in response

--- sodium-api/src/main/java/com/sodium/api/controllers/GroupChatController.java
+++ sodium-api/src/main/java/com/sodium/api/controllers/GroupChatController.java
@@ -8,10 +8,14 @@ import com.sodium.api.models.request.GroupChatCreationRequestBody;
 import com.sodium.api.models.request.GroupChatRequestBody;
 import com.sodium.api.models.response.GroupChatListResponse;
 import com.sodium.api.models.response.GroupChatResponse;
+import com.sodium.api.models.response.AddToGroupChatResponse;
+import com.sodium.api.models.response.UserResponse;
 import com.sodium.api.repositories.DBUserRepository;
 import com.sodium.api.repositories.GroupChatRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,23 +36,33 @@ public class GroupChatController {
     @GetMapping("/api/groups")
     public ResponseEntity<GroupChatListResponse> getGroups() {
         List<GroupChat> groups = groupChatRepository.findAll();
-        GroupChatListResponse groupChatListResponse = new GroupChatListResponse(groups);
 
-        // TODO: remove passwords and personnal info from response
+        GroupChatListResponse groupChatListResponse = new GroupChatListResponse();
+
+        for (GroupChat group : groups) {
+            Set<UserResponse> users = new HashSet<>();
+            for (DBUser user : group.getUsers()) {
+                users.add(new UserResponse(user.getId(), user.getUsername()));
+            }
+            groupChatListResponse.addGroupChat(new GroupChatResponse(group.getId(), group.getName(), users));
+        }
+
+        System.out.println(groupChatListResponse.toString());
 
         return ResponseEntity.ok(groupChatListResponse);
     }
 
     @PostMapping("/api/addToGroupChat")
-    public ResponseEntity<GroupChatResponse> addToGroupChat(@RequestBody GroupChatRequestBody groupChatRequestBody) {
+    public ResponseEntity<AddToGroupChatResponse> addToGroupChat(
+            @RequestBody GroupChatRequestBody groupChatRequestBody) {
         GroupChat groupChat = groupChatRepository.findById(groupChatRequestBody.getGroupChatId()).get();
         if (groupChat == null) {
-            return ResponseEntity.badRequest().body(new GroupChatResponse("Group chat not found", null));
+            return ResponseEntity.badRequest().body(new AddToGroupChatResponse("Group chat not found", null));
         }
 
         DBUser user = userRepository.findById(groupChatRequestBody.getUserId()).get();
         if (user == null) {
-            return ResponseEntity.badRequest().body(new GroupChatResponse("User not found", null));
+            return ResponseEntity.badRequest().body(new AddToGroupChatResponse("User not found", null));
         }
 
         try {
@@ -56,19 +70,19 @@ public class GroupChatController {
             groupChatRepository.save(groupChat);
         } catch (Exception e) {
             System.out.println(e);
-            return ResponseEntity.badRequest().body(new GroupChatResponse("User addition failed", null));
+            return ResponseEntity.badRequest().body(new AddToGroupChatResponse("User addition failed", null));
         }
 
-        return ResponseEntity.ok(new GroupChatResponse("User added to group chat", groupChat.getId()));
+        return ResponseEntity.ok(new AddToGroupChatResponse("User added to group chat", groupChat.getId()));
     }
 
     @PostMapping("/api/createGroupChat")
-    public ResponseEntity<GroupChatResponse> createGroupChat(
+    public ResponseEntity<AddToGroupChatResponse> createGroupChat(
             @RequestBody GroupChatCreationRequestBody groupChatCreationRequestBody) {
         String name = groupChatCreationRequestBody.getName();
 
         if (name == null || name.isEmpty()) {
-            return ResponseEntity.badRequest().body(new GroupChatResponse("Group chat name is required", null));
+            return ResponseEntity.badRequest().body(new AddToGroupChatResponse("Group chat name is required", null));
         }
 
         GroupChat groupChat = new GroupChat(name);
@@ -79,10 +93,10 @@ public class GroupChatController {
             groupChatRepository.save(groupChat);
         } catch (Exception e) {
             System.out.println(e);
-            return ResponseEntity.badRequest().body(new GroupChatResponse("Group chat creation failed", null));
+            return ResponseEntity.badRequest().body(new AddToGroupChatResponse("Group chat creation failed", null));
         }
 
-        return ResponseEntity.ok(new GroupChatResponse("Group chat created", groupChat.getId()));
+        return ResponseEntity.ok(new AddToGroupChatResponse("Group chat created", groupChat.getId()));
     }
 
 }
