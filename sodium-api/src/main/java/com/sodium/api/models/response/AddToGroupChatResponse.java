776dc3e179fd30040752ba03a1b84d1ccddbb53d
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-11 12:04:52 +0200

    feat(Na): create new response models

---
+++ sodium-api/src/main/java/com/sodium/api/models/response/AddToGroupChatResponse.java
@@ -0,0 +1,28 @@
+package com.sodium.api.models.response;
+
+public class AddToGroupChatResponse {
+    private String message;
+    private Integer groupId;
+
+    public AddToGroupChatResponse(String message, Integer groupId) {
+        this.message = message;
+        this.groupId = groupId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+}
---
+++ sodium-api/src/main/java/com/sodium/api/models/response/UserResponse.java
@@ -0,0 +1,27 @@
+package com.sodium.api.models.response;
+
+public class UserResponse {
+    private Integer id;
+    private String username;
+
+    public UserResponse(Integer id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
