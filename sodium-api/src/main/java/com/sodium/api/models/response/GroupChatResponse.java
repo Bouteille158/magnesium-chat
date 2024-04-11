657b821f5045223ee05bcd3bc9ce36de07624d1e
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-11 12:05:19 +0200

    fix(Na): remove password and role from response

--- sodium-api/src/main/java/com/sodium/api/models/response/GroupChatResponse.java
+++ sodium-api/src/main/java/com/sodium/api/models/response/GroupChatResponse.java
@@ -1,28 +1,44 @@
 package com.sodium.api.models.response;
 
+import java.util.Set;
+
 public class GroupChatResponse {
-    private String message;
-    private Integer groupId;
+    private Integer id;
+    private String name;
+    private Set<UserResponse> users;
 
-    public GroupChatResponse(String message, Integer groupId) {
-        this.message = message;
-        this.groupId = groupId;
+    public GroupChatResponse(Integer id, String name, Set<UserResponse> users) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
     }
 
-    public String getMessage() {
-        return message;
+    public Integer getId() {
+        return id;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public String getName() {
+        return name;
     }
 
-    public Integer getGroupId() {
-        return groupId;
+    public Set<UserResponse> getUsers() {
+        return users;
     }
 
-    public void setGroupId(Integer groupId) {
-        this.groupId = groupId;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsers(Set<UserResponse> users) {
+        this.users = users;
+    }
+
+    public void addUser(UserResponse user) {
+        this.users.add(user);
     }
 
 }
