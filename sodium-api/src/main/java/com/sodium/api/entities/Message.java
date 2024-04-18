35e1f4c6d620d29039d7df3435224008eb0bec30
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-18 12:14:14 +0200

    feat(Na): add group chat key to message

--- sodium-api/src/main/java/com/sodium/api/entities/Message.java
+++ sodium-api/src/main/java/com/sodium/api/entities/Message.java
@@ -2,10 +2,15 @@ package com.sodium.api.entities;
 
 import java.time.Instant;
 
+import com.sodium.api.models.GroupChat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Message {
@@ -17,15 +22,19 @@ public class Message {
     private String author;
     private String timestamp;
 
+    @OneToOne
+    @JoinColumn(name = "groupChat")
+    private GroupChat groupChat;
+
     // Default constructor
     public Message() {
     }
 
-    public Message(String text, String author, String timestamp) {
+    public Message(String text, String author, String timestamp, GroupChat groupChat) {
         this.text = text;
         this.author = author;
         this.timestamp = Instant.now().toString();
-
+        this.groupChat = groupChat;
     }
 
     public Long getId() {
@@ -65,4 +74,26 @@ public class Message {
                 ", timestamp='" + timestamp + '\'' +
                 '}';
     }
+
+    public void setGroupChat(GroupChat groupChat) {
+        this.groupChat = groupChat;
+    }
+
+    public GroupChat getGroupChat() {
+        return groupChat;
+    }
+
+    public String getChatName() {
+        return groupChat.getName();
+    }
+
+    public Integer getChatId() {
+        return groupChat.getId();
+    }
+
+    public String getChatInfo() {
+        return "Chat ID: " + groupChat.getId() + "\n" +
+                "Chat Name: " + groupChat.getName();
+    }
+
 }
