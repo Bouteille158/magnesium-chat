5d5bd516bd507a1961f82fd24a79183c4d07663b
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-18 12:15:07 +0200

    feat(Na): create message creation response model

---
+++ sodium-api/src/main/java/com/sodium/api/models/response/MessageCreationResponse.java
@@ -0,0 +1,37 @@
+package com.sodium.api.models.response;
+
+public class MessageCreationResponse {
+    private Long messageId;
+    private String message;
+    private Integer chatId;
+
+    public MessageCreationResponse(Long messageId, String message, Integer chatId) {
+        this.messageId = messageId;
+        this.message = message;
+        this.chatId = chatId;
+    }
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer getChatId() {
+        return chatId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setChatId(Integer chatId) {
+        this.chatId = chatId;
+    }
+}
