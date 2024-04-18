14be9ed7fe30121e0a5d1c1f1da0fe0c9a729bc6
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-18 12:15:48 +0200

    feat(Na): add chat group key on message creation

--- sodium-api/src/main/java/com/sodium/api/controllers/MessageController.java
+++ sodium-api/src/main/java/com/sodium/api/controllers/MessageController.java
@@ -3,9 +3,14 @@ package com.sodium.api.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sodium.api.entities.Message;
+import com.sodium.api.models.GroupChat;
+import com.sodium.api.models.request.MessageCreationRequest;
+import com.sodium.api.models.response.MessageCreationResponse;
+import com.sodium.api.repositories.GroupChatRepository;
 import com.sodium.api.repositories.MessageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,17 +20,30 @@ public class MessageController {
     @Autowired
     private MessageRepository messageRepository;
 
+    @Autowired
+    private GroupChatRepository groupChatRepository;
+
+    // TODO: add admin in route
     @GetMapping("/messages")
     public Iterable<Message> getMessages() {
         return messageRepository.findAll();
     }
 
     @PostMapping("/messages")
-    public Message createMessage(@RequestBody Message message) {
+    public ResponseEntity<MessageCreationResponse> createMessage(@RequestBody MessageCreationRequest request) {
+        // TODO: check if user is in the chatgroup
+        Message message = request.getMessage();
         if (message.getId() != null) {
             throw new IllegalArgumentException("You cannot create a message with an ID");
         }
-        return messageRepository.save(message);
+        GroupChat groupChat = groupChatRepository.findById(request.getChatId()).orElseThrow();
+        message.setGroupChat(groupChat);
+        message.getChatInfo();
+
+        Message savedMessage = messageRepository.save(message);
+
+        return ResponseEntity.ok(new MessageCreationResponse(savedMessage.getId(), savedMessage.getText(),
+                savedMessage.getGroupChat().getId()));
     }
 
 }
