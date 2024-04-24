6257025773594249cf2174bc9030ed813935381d
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-24 12:20:45 +0200

    feat(Na): create getUserGroups route
    get userId from token

--- sodium-api/src/main/java/com/sodium/api/controllers/GroupChatController.java
+++ sodium-api/src/main/java/com/sodium/api/controllers/GroupChatController.java
@@ -4,27 +4,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sodium.api.models.DBUser;
 import com.sodium.api.models.GroupChat;
+import com.sodium.api.models.UserGroup;
 import com.sodium.api.models.requests.GroupChatCreationRequestBody;
 import com.sodium.api.models.requests.GroupChatRequestBody;
 import com.sodium.api.models.responses.AddToGroupChatResponse;
 import com.sodium.api.models.responses.GroupChatListResponse;
 import com.sodium.api.models.responses.GroupChatResponse;
+import com.sodium.api.models.responses.UserGroupsResponse;
 import com.sodium.api.models.responses.UserResponse;
 import com.sodium.api.repositories.DBUserRepository;
 import com.sodium.api.repositories.GroupChatRepository;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 public class GroupChatController {
+
+    @Value("${rsa.private.key}")
+    private String jwtKey;
 
     @Autowired
     private GroupChatRepository groupChatRepository;
@@ -50,6 +63,35 @@ public class GroupChatController {
         System.out.println(groupChatListResponse.toString());
 
         return ResponseEntity.ok(groupChatListResponse);
+    }
+
+    @GetMapping("/api/getUserGroups")
+    public ResponseEntity<UserGroupsResponse> getUserGroups(@RequestHeader("Authorization") String token) {
+        UserGroupsResponse userGroupsResponse = new UserGroupsResponse();
+
+        // Supprimez le préfixe "Bearer " du token
+        token = token.replace("Bearer ", "");
+
+        SecretKey secretKey = new SecretKeySpec(jwtKey.getBytes(StandardCharsets.UTF_8), "RSA");
+
+        // Récupère l'ID de l'utilisateur à partir du token
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
+                secretKey)
+                .build();
+
+        // Décoder le token
+        Jwt jwt = jwtDecoder.decode(token);
+
+        // Récupère l'ID de l'utilisateur à partir du token
+        Long userId = jwt.getClaim("userId");
+
+        List<GroupChat> groups = groupChatRepository.findByUsersId(userId);
+
+        for (GroupChat group : groups) {
+            userGroupsResponse.addGroupChat(new UserGroup(group.getId(), group.getName()));
+        }
+
+        return ResponseEntity.ok(userGroupsResponse);
     }
 
     @PostMapping("/api/addToGroupChat")
--- sodium-api/src/main/java/com/sodium/api/repositories/GroupChatRepository.java
+++ sodium-api/src/main/java/com/sodium/api/repositories/GroupChatRepository.java
@@ -1,8 +1,12 @@
 package com.sodium.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sodium.api.models.GroupChat;
 
 public interface GroupChatRepository extends JpaRepository<GroupChat, Integer> {
+
+    List<GroupChat> findByUsersId(Long int1);
 
 }
