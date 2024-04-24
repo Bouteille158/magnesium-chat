33a16d2654d388fb3325f34aadbadce057d50423
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-04-24 12:19:30 +0200

    feat(Na): add groups to user groups response

--- sodium-api/src/main/java/com/sodium/api/models/responses/UserGroupsResponse.java
+++ sodium-api/src/main/java/com/sodium/api/models/responses/UserGroupsResponse.java
@@ -1,9 +1,31 @@
 package com.sodium.api.models.responses;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.sodium.api.models.UserGroup;
+
 public class UserGroupsResponse {
+    private Set<UserGroup> userGroups;
 
-    public void addGroupChat(GroupChatResponse groupChatResponse) {
+    public void addGroupChat(UserGroup userGroup) {
+        this.userGroups.add(userGroup);
+    }
 
+    public Set<UserGroup> getUserGroups() {
+        return userGroups;
+    }
+
+    public void setUserGroups(Set<UserGroup> userGroups) {
+        this.userGroups = userGroups;
+    }
+
+    public UserGroupsResponse() {
+        this.userGroups = new HashSet<>();
+    }
+
+    public UserGroupsResponse(Set<UserGroup> userGroups) {
+        this.userGroups = userGroups;
     }
 
 }
